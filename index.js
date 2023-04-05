7c3f5006c5d7ffdeda0a270bad04f2a2103b8839
Chris Shino <chris.shino@shopify.com>
2023-04-05 09:34:48 -0400

    try with env

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const env = process.env
+  console.log(env)
 } catch (error) {
   core.setFailed(error.message);
 }
